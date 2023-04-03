8352f44222508cc56d075618d162f5b0b98a16e8
Palm <palmpasuthorn@gmail.com>
2023-04-04 03:58:11 +0700

    chore: add tools and multiple package manager support

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,29 @@
-import { defineConfig } from "rollup";
-import { aliucordPlugin, makeManifest, makePluginZip } from "@aliucord/rollup-plugin";
+import {
+    aliucordPlugin,
+    makeManifest,
+    makePluginZip,
+} from '@aliucord/rollup-plugin';
+import { defineConfig } from 'rollup';
+
+const { plugin, engineDir } = process.env;
+if (!engineDir || !plugin) console.error('Incomplete environment variables');
+
+const pluginDir = `./plugins/${plugin}`;
 
 export default defineConfig({
-    input: `${process.env.plugin}/index.ts`,
+    input: `${pluginDir}/index.ts`,
     output: {
-        file: `dist/${process.env.plugin}.js`
+        file: `dist/${plugin}.js`,
     },
     plugins: [
         aliucordPlugin({
-            autoDeploy: !!process.env.ROLLUP_WATCH,
-            hermesPath: "node_modules/.pnpm/hermes-engine@0.11.0/node_modules/hermes-engine"
+            autoDeploy: !!process.env['ROLLUP_WATCH'],
+            hermesPath: engineDir!,
         }),
         makeManifest({
-            baseManifest: "baseManifest.json",
-            manifest: `${process.env.plugin}/manifest.json`
-	}),
-        makePluginZip()
-    ]
+            baseManifest: 'manifest.json',
+            manifest: `${pluginDir}/manifest.json`,
+        }),
+        makePluginZip(),
+    ],
 });
